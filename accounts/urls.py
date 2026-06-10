f12c5c4cd430ca79e8ffe7bdb30338efd7f4ef5b
picker770 <arehman897@gmail.com>
2026-06-10 19:38:16 +0100

    Add public profile view page

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/<str:username>/', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import CustomUserCreationForm
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 def register(request):
@@ -16,5 +21,9 @@ def register(request):
         form = CustomUserCreationForm()
 
     return render(request, 'accounts/register.html', {'form': form})
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    return render(request, 'accounts/profile.html', {'profile_user': user})
 
 
