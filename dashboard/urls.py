b4c9badbb78cd38483be8a394aa520b71eca7e0e
picker770 <arehman897@gmail.com>
2026-06-11 11:31:19 +0100

    Add dashboard placeholder with login requirement

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
-urlpatterns = []
+app_name = 'dashboard'
+
+urlpatterns = [
+    path('', login_required(TemplateView.as_view(template_name='dashboard/home.html')), name='home'),
+    
+]
--- fitness_platform/urls.py
+++ fitness_platform/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/', include('accounts.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
 
