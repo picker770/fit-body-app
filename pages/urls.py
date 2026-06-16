2d9e3dcad39d21c022102c76e4230ad4a107e697
picker770 <arehman897@gmail.com>
2026-06-16 18:34:04 +0100

    Fix contact form: render fields directly in template

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 app_name = 'pages'
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name='pages/about.html'), name='about'),
     path('features/', TemplateView.as_view(template_name='pages/features.html'), name='features'),
     path('pricing/', TemplateView.as_view(template_name='pages/pricing.html'), name='pricing'),
+    path('contact/', views.contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process the form data (you can add email sending here later)
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            
+            # For now, just show success message
+            messages.success(request, f'Thank you {name}! We will get back to you soon.')
+            return redirect('pages:contact')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+            return render(request, 'pages/contact.html', {'form': form})
+    else:
+        form = ContactForm()
+    
+    return render(request, 'pages/contact.html', {'form': form})
