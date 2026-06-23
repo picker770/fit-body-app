2e34076ff9dfd40238dd9e71265ece02d20fbc73
picker770 <arehman897@gmail.com>
2026-06-23 10:42:21 +0100

    fix unclosed tag in post_detail.html, and missing endblock for extra_js block, and also image upload working in post progress

--- fitness_platform/urls.py
+++ fitness_platform/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -7,9 +9,15 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
-    path('', include('pages.urls')),
+    path('pages/', include('pages.urls')),
     path('products/', include('products.urls')),
     path('payments/', include('payments.urls')),
     path('community/', include('community.urls')),
 ]
+
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
