fcb38a6b5ddd6971befb2ed09c650cded365286a
picker770 <arehman897@gmail.com>
2026-06-23 08:49:49 +0100

    Add ProgressPost and Comment models for community features and fix the typo on get_user_model

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import ProgressPost, Comment
 
-# Register your models here.
+
+@admin.register(ProgressPost)
+class ProgressPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'total_likes')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'caption', 'user__username')
+    readonly_fields = ('created_at', 'updated_at')
+
+    def total_likes(self, obj):
+        return obj.total_likes()
+    total_likes.short_description = 'Likes'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'content', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('content', 'user__username', 'post__title')
+
+
