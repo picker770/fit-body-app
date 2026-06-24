e194c1a494add3af86afcc6554482d02015ab743
picker770 <arehman897@gmail.com>
2026-06-24 08:13:25 +0100

    Fix: Update ALLOWED_HOSTS to accept any .herokuapp.com domain

--- fitness_platform/settings.py
+++ fitness_platform/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.herokuapp.com', 
+]
 
 
 # Application definition
